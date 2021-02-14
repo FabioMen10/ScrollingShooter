3def81bc130fd571f543b021fbfb11adc203bc20
Fábio Mendes <fabiomendesilva@gmail.com>
2021-02-14 17:29:34 -0400

    Renamed game title inside code.

--- conf.lua
+++ conf.lua
@@ -1,8 +1,7 @@
 -- Configuration
 function love.conf(t)
-  t.title = "Scrolling Shooter" -- The title of the window the game is in (string)
-  t.version = "11.2" -- The LÖVE version this game was made for (string)
+  t.title = "Sonic Lovings" -- The title of the window the game is in (string)
+  t.version = "11.3" -- The LÖVE version this game was made for (string)
   t.window.width = 445 -- we want our game to be long and thin.
   t.window.height = 720
-
 end
